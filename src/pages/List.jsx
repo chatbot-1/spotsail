495d2af4f79b6fd2179b618d59b755c52d9f1f04
Atul Singh <getupmaverick@gmail.com>
2023-12-18 14:58:22 +0530

    new commit

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import study from "../assets/study.gif"
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="container mt-[150px]">
+      <div>
+        <div className="flex justify-center gap-5 items-center">
+          <img src={study} alt="" className="h-[80px]"/>
+          <h1 className="text-[40px] font-bold">List your industry</h1>
+        </div>
+        <div className="flex shadow-[inset_0px_20px_20px_10px_#00000024] w-[600px] m-auto p-10 flex-col justify-center items-center mt-[50px]">
+          <span>
+            Note: Make sure you enter the correct data for verification
+          </span>
+          <div className="flex flex-col justify-around mt-[40px] gap-11">
+            <div className="flex justify-between gap-5">
+              <p>Name: </p>
+              <input
+                type="text"
+                className="bg-transparent w-[300px] outline-none border-solid border-[1px] border-gray-400 px-[10px] py-[5px] rounded"
+              />
+            </div>
+            <div className="flex justify-between gap-5">
+              <p>Email: </p>
+              <input
+                type="email"
+                className="bg-transparent w-[300px] outline-none border-solid border-[1px] border-gray-400 px-[10px] py-[5px] rounded"
+              />
+            </div>
+            <div className="flex justify-between gap-5">
+              <p>Contact No: </p>
+              <input
+                type="number"
+                className="bg-transparent w-[300px] outline-none border-solid border-[1px] border-gray-400 px-[10px] py-[5px] rounded"
+              />
+            </div>
+            <div className="flex justify-between gap-5">
+              <p>Address: </p>
+              <textarea
+                name=""
+                id=""
+                rows="4"
+                className="bg-transparent w-[300px] outline-none border-solid border-[1px] border-gray-400 px-[10px] py-[5px] rounded"
+              ></textarea>
+            </div>
+            <div className="flex justify-between gap-5">
+              <p>Upload Cover: </p>
+              <input
+                type="file"
+                accept=".png, .jpg, .jpeg"
+                className="bg-transparent w-[300px] outline-none border-solid border-[1px] border-gray-400 px-[10px] py-[5px] rounded"
+              />
+            </div>
+          </div>
+          <button className="mt-[40px] mb-[10px] p-1 bg-gray-900/30 px-[30px] py-[10px] rounded font-bold shadow-[inset_0px_20px_20px_10px_#00000024] flex gap-2 items-center justify-center">Submit</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
--- src/pages/profiles/UserProfile.jsx
+++ src/pages/profiles/UserProfile.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from "react";
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
-  )
-}
+    <div className="mt-[130px]">
+      <div className="flex shadow-[inset_0px_20px_20px_10px_#00000024] bg-black h-[70vh]">
+        <div>
+          <div className="border-r-[2px] w-[200px] p-6 h-full">
+            <div className=" flex flex-col gap-9">
+              <h1 className="mb-[10px]">Heyy user name</h1>
+              <h1 className="p-2 cursor-pointer rounded shadow-[inset_0px_0px_131px_0px_#2d3748]">
+                Dashboard
+              </h1>
+              <h1 className="p-2 cursor-pointer rounded shadow-[inset_0px_0px_131px_0px_#2d3748]">
+                User Profile
+              </h1>
+              <h1 className="p-2 cursor-pointer rounded shadow-[inset_0px_0px_131px_0px_#2d3748]">
+                Logout
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="w-full p-6">
+          <h1 className="text-center">Your Spotsail status</h1>
+          <div className="flex shadow-[inset_0px_0px_131px_0px_#2d3748] justify-between items-center mt-[50px] rounded p-4">
+            <div>
+              <p className="p-2 bg-gray-800 h-[40px] w-[40px] flex items-center justify-center rounded-full">
+                1
+              </p>
+            </div>
+            <div className="leading-5">
+              <p className="text-[18px]">Restaurant Name</p>
+              <span className="text-[12px]">restaurant</span>
+            </div>
+            <div>
+              <p className="text-[18px] text-green-500">Confirmed</p>
+              <p className="text-[12px]"> Date</p>
+            </div>
+            <div>
+              <p className="text-[18px]">Paid</p>
+              <p className="text-[12px]">Amount</p>
+            </div>
+          </div>
+          <div className="flex shadow-[inset_0px_0px_131px_0px_#2d3748] justify-between items-center mt-[50px] rounded p-4">
+            <div>
+              <p className="p-2 bg-gray-800 h-[40px] w-[40px] flex items-center justify-center rounded-full">
+                1
+              </p>
+            </div>
+            <div className="leading-5">
+              <p className="text-[18px]">Restaurant Name</p>
+              <span className="text-[12px]">restaurant</span>
+            </div>
+            <div>
+              <p className="text-[18px] text-green-500">Confirmed</p>
+              <p className="text-[12px]"> Date</p>
+            </div>
+            <div>
+              <p className="text-[18px]">Paid</p>
+              <p className="text-[12px]">Amount</p>
+            </div>
+          </div>
 
-export default UserProfile
+          <div className="flex shadow-[inset_0px_0px_131px_0px_#2d3748] justify-between items-center mt-[50px] rounded p-4">
+            <div>
+              <p className="p-2 bg-gray-800 h-[40px] w-[40px] flex items-center justify-center rounded-full">
+                1
+              </p>
+            </div>
+            <div className="leading-5">
+              <p className="text-[18px]">Restaurant Name</p>
+              <span className="text-[12px]">restaurant</span>
+            </div>
+            <div>
+              <p className="text-[18px] text-green-500">Confirmed</p>
+              <p className="text-[12px]"> Date</p>
+            </div>
+            <div>
+              <p className="text-[18px]">Paid</p>
+              <p className="text-[12px]">Amount</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
